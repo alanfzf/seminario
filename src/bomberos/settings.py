1311e2eba725ae09d49749e926f71ea2bd48fdc7
alanfvn <alandavidg16@gmail.com>
2023-10-16 00:59:23 -0600

    Preparing static files

--- src/bomberos/settings.py
+++ src/bomberos/settings.py
@@ -129,7 +129,9 @@ DATE_FORMAT = 'Y-m-d'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
